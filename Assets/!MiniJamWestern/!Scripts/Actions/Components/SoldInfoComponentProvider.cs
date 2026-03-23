daba29f010d86fd2207e9b19eb823d4a9b3399af
Kirill <dr.mokrousoff2017@yandex.ru>
2026-03-24 01:25:25 +0300

    Перенос файлов

---
+++ Assets/!MiniJamWestern/!Scripts/Actions/Components/SoldInfoComponentProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BitterECS.Integration.Unity;
+using UnityEngine;
+
+public class SoldInfoComponentProvider : ProviderEcs<SoldInfoComponent>
+{
+
+}
+
+[Serializable]
+public struct SoldInfoComponent
+{
+    public Sprite icon;
+    [TextArea(1, 3)]
+    public string title;
+    [TextArea(1, 5)]
+    public string description;
+    public int amount;
+}
