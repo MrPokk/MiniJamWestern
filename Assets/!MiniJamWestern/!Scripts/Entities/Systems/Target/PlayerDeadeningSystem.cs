bcc2dc5afb5efc5f33d7a080ca676518c1236bf0
kirill <dr.mokrousoff2017@yandex.ru>
2026-03-25 16:06:59 +0300

    Сделай смерть

--- Assets/!MiniJamWestern/!Scripts/Entities/Systems/Target/PlayerDeadeningSystem.cs
+++ Assets/!MiniJamWestern/!Scripts/Entities/Systems/Target/PlayerDeadeningSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using BitterECS.Core;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerDeadeningSystem : IEcsAutoImplement
@@ -10,6 +11,10 @@ public class PlayerDeadeningSystem : IEcsAutoImplement
     private EcsEvent _ecsEvent = new EcsEvent().SubscribeWhereEntity<IsDeadEvent>(e => e.Has<TagPlayer>(), added: OnPlayerDead);
     private static void OnPlayerDead(EcsEntity entity)
     {
+
+        Debug.Log("Player is Dead next restart ");
+        //TODO: ADD restart 
+
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
